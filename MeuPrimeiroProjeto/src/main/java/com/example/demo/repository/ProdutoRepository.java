c1817da971e4bdda3b0accc65eeb17ee0c05730c
antonioxavier <antonioxavierdesousaneto@gmail.com>
2019-03-11 10:36:08 -0300

    agenda completa com spring e vuejs

---
+++ MeuPrimeiroProjeto/src/main/java/com/example/demo/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	Produto	findById(long id);
+
+}
